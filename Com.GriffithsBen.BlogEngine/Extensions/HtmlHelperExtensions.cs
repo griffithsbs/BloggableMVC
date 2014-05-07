f9d6c026289671e6c2dd6d3374e949c1db4d0088
Ben Griffiths <benstephengriffiths@gmail.com>
2014-05-07 20:44:01 +0100

    adding HtmlHelperExtensions class with method stubs

---
+++ Com.GriffithsBen.BlogEngine/Extensions/HtmlHelperExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Com.GriffithsBen.BlogEngine.Extensions {
+
+    public static class HtmlHelperExtensions {
+
+        public static MvcHtmlString BlogContentFor(this HtmlHelper helper) {
+            // TODO take in an expression plus html attributes
+            return null;
+        }
+
+        public static MvcHtmlString BlogContent(this HtmlHelper helper, string content) {
+            // TODO take in html attributes
+            return null;
+        }
+
+    }
+
+}
