b9cef02a6ed7345d5f2759ea6206341bb2f0fae2
Ben Griffiths <benstephengriffiths@gmail.com>
2014-05-10 12:43:43 +0100

    removing unused css and js libraries from sample client project

--- Com.GriffithsBen.BloggableMVC.SampleClient/App_Start/BundleConfig.cs
+++ Com.GriffithsBen.BloggableMVC.SampleClient/App_Start/BundleConfig.cs
@@ -8,14 +8,8 @@ namespace Com.GriffithsBen.BloggableMVC.SampleClient {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
