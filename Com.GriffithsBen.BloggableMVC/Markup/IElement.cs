089d9595b7f3aa8b62872f12bd488f39b899ccc6
Ben Griffiths <benstephengriffiths@gmail.com>
2014-06-01 23:16:00 +0100

    adding IElement interface

---
+++ Com.GriffithsBen.BloggableMVC/Markup/IElement.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace Com.GriffithsBen.BloggableMVC.Markup {
+
+    interface IElement {
+
+        int GetTextLength();
+
+        IElement Truncate(int textEndIndex, string textToAppend = "...");
+
+        MvcHtmlString GetHtml();
+
+        Element Clone(); // TODO - does this need to be in this interface?
+
+        bool IsValid();
+
+    }
+
+}
